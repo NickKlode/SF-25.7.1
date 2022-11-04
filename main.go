074c0bf40c0f4f6ea7b0b81bab2621bd7fab2449
Никита К <nickklode@MacBook-Air-Nikita.local>
2022-11-04 18:55:39 +0300

    feature

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	nums := []int{1, 2, 3, 4}
 	for i := range nums {
-		fmt.Printf("Число - %d\n", nums[i])
+		fmt.Printf("Число - %d, Квадрат чис - %d\n", nums[i], nums[i]*nums[i])
+
 	}
 }
